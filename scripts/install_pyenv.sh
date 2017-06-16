893872676693d4983b62d9c68ca67985cdaf29d3
Samil Goh <iam312@gmail.com>
2017-06-17 00:02:56 +0900

    pyenv-update 플러그인 추가

--- scripts/install_pyenv.sh
+++ scripts/install_pyenv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+git clone git://github.com/yyuu/pyenv-update.git ~/.pyenv/plugins/pyenv-update
 pyenv install 2.7.12
